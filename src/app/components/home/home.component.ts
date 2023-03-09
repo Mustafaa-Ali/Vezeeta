472f5d8dcf21ac2b7ef789111d8f182838266190
AmanyAsaad <amanyasad88@gmail.com>
2023-03-09 15:27:41 +0200

    updateHome

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,15 @@
 import { IDoctor } from './../../models/IDoctor';
 import { Component, OnInit } from '@angular/core';
 import { DoctorsService } from 'src/app/services/doctors.service';
-
+import {Observable} from 'rxjs';
 import { CityService } from 'src/app/services/city.service';
 import { ICity } from 'src/app/models/ICity';
 import { IOffer } from 'src/app/models/IOffer';
 import { OfferService } from 'src/app/services/offer.service';
 import { ISpeciality } from 'src/app/models/ISpeciality';
 import { SpecialityService } from 'src/app/services/speciality.service';
+import { FormControl } from '@angular/forms';
+import {map, startWith} from 'rxjs/operators';
 
 
 @Component({
@@ -24,9 +26,12 @@ export class HomeComponent {
   speciality:ISpeciality [] =[];
   selectedCityID:number=0;
   selectedSpecialityID:number=0;
+  specialityOptions: string[] = [];
+  myControl = new FormControl('');
+  filteredOptions: Observable<string[]>;
 
   constructor(private DS: DoctorsService, private CS:CityService, private OS:OfferService, private SS:SpecialityService) {
-
+     this.filteredOptions = new Observable();
   }
  
 
@@ -50,9 +55,23 @@ export class HomeComponent {
    this.SS.getSpecilaty().subscribe((data)=>{
     console.log("speciality",data);
     this.speciality = data;
+    data.map((item)=>{
+      console.log("item",item);
+      this.specialityOptions.push(item.Name);
+    })
    })
+
+
+
     
+
+  console.log("fillterd",this.filteredOptions)
   }
+
+
+
+
+  
   getCities(){
    
 
